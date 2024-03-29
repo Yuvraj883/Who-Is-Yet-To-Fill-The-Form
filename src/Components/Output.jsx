f22addf265dba3329b32caba882ee811ddaf829e
Yuvraj <devyuvraj883@gmail.com>
2024-03-29 13:02:02 +0530

    Updated style

--- src/Components/Output.jsx
+++ src/Components/Output.jsx
@@ -1,21 +1,49 @@
-const Output = (props)=>{
+import { IoMdCopy } from 'react-icons/io'
+import { IoIosCopy } from 'react-icons/io'
+import { useState } from 'react'
+
+const Output = (props) => {
+  const [copied, setCopied] = useState(false)
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(props.hasNotFilledList)
+      setCopied(true)
+    } catch (error) {
+      alert(error)
+    }
+  }
+
   return (
     <>
-    <section className="flex flex-col items-center justify-center">
-      <ol>
-        {
-          props.hasNotFilledList.map((name)=>(
-            <li>{name}</li>
-          ))
-        }
-      </ol>
-      {
-        props.hasNotFilledList.length===0 &&
-        <div>Everyone has filled</div>
-      }
-    </section>
+      <section className='flex flex-col items-center justify-center h-60   p-4 mt-8'>
+        {props.hasNotFilledList.length > 0 && (
+          <div className='flex flex-col w-80 shadow-md p-4'>
+            {!copied && (
+              <IoMdCopy
+                className='ml-auto cursor-pointer'
+                onClick={copyToClipboard}
+              />
+            )}
+            {copied && <IoIosCopy className='ml-auto' />}
+            <h1 className='font-bold italic'>
+              These students are yet to fill the form:- <br />
+            </h1>
 
+            <ol>
+              {props.hasNotFilledList.map((name) => (
+                <li>{name}</li>
+              ))}
+            </ol>
+          </div>
+        )}
+        {props.hasNotFilledList.length === 0 && (
+          <div className='font-bold'>
+            Congratulations!! Everyone has filled the form.
+          </div>
+        )}
+      </section>
     </>
   )
 }
-export default Output;
+export default Output
